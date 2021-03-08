d7431db88a9ad3723d9a0feb18738ba63f2d263b
hamouda ben abdennebi <hamoudabenabdennebi77@gmail.com>
2021-03-08 22:25:55 +0100

    first commit

--- server.js
+++ server.js
@@ -8,23 +8,23 @@ app.use(express.static(__dirname + "/public/javascript"));
 app.use(express.static(__dirname + "/public/style"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-  });
-  app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/about.html"));
-  });
-  app.get("/application", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/app.html"));
-  });
-  app.get("/social", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/social.html"));
-  });
-  app.get("/rules", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/rules.html"));
-  });
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/about.html"));
+});
+app.get("/application", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/app.html"));
+});
+app.get("/social", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/social.html"));
+});
+app.get("/rules", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/rules.html"));
+});
 
 
 
-app.listen(PORT,()=>{
-    console.log(PORT);
+app.listen(PORT, () => {
+  console.log(PORT);
 });
